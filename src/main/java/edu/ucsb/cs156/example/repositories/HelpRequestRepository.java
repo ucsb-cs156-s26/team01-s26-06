60a5ffaa8af28e5e1412eb23ed29b21bdbfe3c13
Chaewon <cbang@ucsb.edu>
2026-04-28 14:41:57 -0700

    cb - deleted comments inside the HelpRequestRepository

--- src/main/java/edu/ucsb/cs156/example/repositories/HelpRequestRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/HelpRequestRepository.java
@@ -8,14 +8,4 @@ import org.springframework.stereotype.Repository;
 /** The HelpRequestRepository is a repository for HepRequest entities. */
 @Repository
 @RepositoryRestResource(exported = false)
-public interface HelpRequestRepository extends CrudRepository<HelpRequest, Long> {
-  // /**
-  //  * This method returns all UCSBDate entities with a given quarterYYYYQ.
-  //  *
-  //  * @param quarterYYYYQ quarter in the format YYYYQ (e.g. 20241 for Winter 2024, 20242 for
-  // Spring
-  //  *     2024, 20243 for Summer 2024, 20244 for Fall 2024)
-  //  * @return all UCSBDate entities with a given quarterYYYYQ
-  //  */
-  // Iterable<UCSBDate> findAllByQuarterYYYYQ(String quarterYYYYQ);
-}
+public interface HelpRequestRepository extends CrudRepository<HelpRequest, Long> {}
